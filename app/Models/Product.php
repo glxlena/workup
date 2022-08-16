43264380725e71b2007adc39abacb740adce3801
Helena <lena.oli1102@gmail.com>
2022-08-16 09:58:58 -0300

    atualização 1

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -20,4 +20,8 @@ class Product extends Model
     {
       return $this->belongsTo(Establishment::class);
     }
+    public function menus()
+    {
+      return $this->belongsToMany(Menu::class);
+    }
 }
